1b4a6dbc9eee361342af2d1c1ac5c2c1fa55abc4
star-dark <den1116@naver.com>
2024-12-28 19:28:12 +0900

    asdasdd

--- src/main/java/net/mcreator/palworld/procedures/Test1Procedure.java
+++ src/main/java/net/mcreator/palworld/procedures/Test1Procedure.java
@@ -1,6 +1,24 @@
 package net.mcreator.palworld.procedures;
 
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.client.gui.components.EditBox;
+
+import java.util.HashMap;
+
 public class Test1Procedure {
-	public static void execute() {
+	public static void execute(Entity entity, HashMap guistate) {
+		if (entity == null || guistate == null)
+			return;
+		if (guistate.get("text:textFieldName") instanceof EditBox _tf)
+			_tf.setValue("Text");
+		if (entity instanceof Player _player) {
+			_player.getAbilities().mayfly = true;
+			_player.onUpdateAbilities();
+		}
+		if (entity instanceof Player _player) {
+			_player.getAbilities().mayfly = true;
+			_player.onUpdateAbilities();
+		}
 	}
 }
