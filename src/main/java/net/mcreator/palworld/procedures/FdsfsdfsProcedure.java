ee6fa0f680f9f796b287e24c117ba0158d2ebfc3
ShinSeungHyuck <1222ssh@naver.com>
2024-12-28 19:24:01 +0900

    yeahhhh2

--- src/main/java/net/mcreator/palworld/procedures/FdsfsdfsProcedure.java
+++ src/main/java/net/mcreator/palworld/procedures/FdsfsdfsProcedure.java
@@ -1,6 +1,8 @@
 package net.mcreator.palworld.procedures;
 
-import net.neoforged.bus.api.Event;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.network.chat.Component;
 
 public class FdsfsdfsProcedure {
 	public static void execute(Entity entity) {
--- src/main/java/net/mcreator/palworld/procedures/Test3Procedure.java
+++ src/main/java/net/mcreator/palworld/procedures/Test3Procedure.java
@@ -1,6 +1,16 @@
 package net.mcreator.palworld.procedures;
 
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.network.chat.Component;
+
 public class Test3Procedure {
-	public static void execute() {
+	public static void execute(Entity entity) {
+		if (entity == null)
+			return;
+		if (true) {
+			if (entity instanceof Player _player && !_player.level().isClientSide())
+				_player.displayClientMessage(Component.literal("Message"), false);
+		}
 	}
 }
