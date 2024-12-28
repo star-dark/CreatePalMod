a556535bd6aa7f439750de1cc98277586b5e938d
star-dark <den1116@naver.com>
2024-12-28 19:24:16 +0900

    sadasd

--- src/main/java/net/mcreator/palworld/procedures/Test3Procedure.java
+++ src/main/java/net/mcreator/palworld/procedures/Test3Procedure.java
@@ -1,6 +1,14 @@
 package net.mcreator.palworld.procedures;
 
+import net.minecraft.client.gui.components.EditBox;
+
+import java.util.HashMap;
+
 public class Test3Procedure {
-	public static void execute() {
+	public static void execute(HashMap guistate) {
+		if (guistate == null)
+			return;
+		if (guistate.get("text:textFieldName") instanceof EditBox _tf)
+			_tf.setValue("Text");
 	}
 }
