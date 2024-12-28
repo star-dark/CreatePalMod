97a77f06d206007cc4016b95eab71bd064a612f5
ShinSeungHyuck <1222ssh@naver.com>
2024-12-28 20:00:27 +0900

    Farmer Finish

--- src/main/java/net/mcreator/palworld/init/PalworldModItems.java
+++ src/main/java/net/mcreator/palworld/init/PalworldModItems.java
@@ -10,6 +10,9 @@ import net.neoforged.neoforge.common.DeferredSpawnEggItem;
 
 import net.minecraft.world.item.Item;
 
+import net.mcreator.palworld.item.FarmerSporeItem;
+import net.mcreator.palworld.item.FarmerRootItem;
+import net.mcreator.palworld.item.FarmerDregsItem;
 import net.mcreator.palworld.PalworldMod;
 
 public class PalworldModItems {
@@ -17,6 +20,9 @@ public class PalworldModItems {
 	public static final DeferredItem<Item> FARMER_JOB_NPC_SPAWN_EGG = REGISTRY.register("farmer_job_npc_spawn_egg", () -> new DeferredSpawnEggItem(PalworldModEntities.FARMER_JOB_NPC, -1, -1, new Item.Properties()));
 	public static final DeferredItem<Item> FISHER_JOB_NPC_SPAWN_EGG = REGISTRY.register("fisher_job_npc_spawn_egg", () -> new DeferredSpawnEggItem(PalworldModEntities.FISHER_JOB_NPC, -1, -1, new Item.Properties()));
 	public static final DeferredItem<Item> MINER_JOB_NPC_SPAWN_EGG = REGISTRY.register("miner_job_npc_spawn_egg", () -> new DeferredSpawnEggItem(PalworldModEntities.MINER_JOB_NPC, -1, -1, new Item.Properties()));
+	public static final DeferredItem<Item> FARMER_DREGS = REGISTRY.register("farmer_dregs", FarmerDregsItem::new);
+	public static final DeferredItem<Item> FARMER_ROOT = REGISTRY.register("farmer_root", FarmerRootItem::new);
+	public static final DeferredItem<Item> FARMER_SPORE = REGISTRY.register("farmer_spore", FarmerSporeItem::new);
 	// Start of user code block custom items
 	// End of user code block custom items
 }
---
+++ src/main/java/net/mcreator/palworld/item/FarmerRootItem.java
@@ -0,0 +1,11 @@
+
+package net.mcreator.palworld.item;
+
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+
+public class FarmerRootItem extends Item {
+	public FarmerRootItem() {
+		super(new Item.Properties().stacksTo(32).rarity(Rarity.COMMON));
+	}
+}
