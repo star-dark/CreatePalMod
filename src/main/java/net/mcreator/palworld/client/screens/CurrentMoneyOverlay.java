12c07a46645beaf2aaa27f261944598780cec0da
star-dark <den1116@naver.com>
2025-01-05 14:50:54 +0900

    gui수정

--- src/main/java/net/mcreator/palworld/client/screens/CurrentMoneyOverlay.java
+++ src/main/java/net/mcreator/palworld/client/screens/CurrentMoneyOverlay.java
@@ -1,6 +1,9 @@
 
 package net.mcreator.palworld.client.screens;
 
+import org.joml.Vector3f;
+import org.joml.Quaternionf;
+
 import org.checkerframework.checker.units.qual.h;
 
 import net.neoforged.neoforge.client.event.RenderGuiEvent;
@@ -11,10 +14,14 @@ import net.neoforged.api.distmarker.Dist;
 
 import net.minecraft.world.level.Level;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.gui.screens.inventory.InventoryScreen;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.Minecraft;
 
+import net.mcreator.palworld.procedures.UserFaceProcedure;
 import net.mcreator.palworld.procedures.BringMyMoneyProcedure;
 
 import com.mojang.blaze3d.systems.RenderSystem;
@@ -44,16 +51,41 @@ public class CurrentMoneyOverlay {
 		RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
 		RenderSystem.setShaderColor(1, 1, 1, 1);
 		if (true) {
-			event.getGuiGraphics().blit(ResourceLocation.parse("palworld:textures/screens/money2.png"), 0, 0, 0, 0, 32, 32, 32, 32);
+			event.getGuiGraphics().blit(ResourceLocation.parse("palworld:textures/screens/money2.png"), 41, 5, 0, 0, 32, 32, 32, 32);
 
 			event.getGuiGraphics().drawString(Minecraft.getInstance().font,
 
-					BringMyMoneyProcedure.execute(entity), 33, 11, -1, false);
+					BringMyMoneyProcedure.execute(entity), 74, 16, -1, false);
+			if (UserFaceProcedure.execute(entity) instanceof LivingEntity livingEntity) {
+				renderEntityInInventoryFollowsAngle(event.getGuiGraphics(), 22, 31, 30, 0f, 0, livingEntity);
+			}
 		}
 		RenderSystem.depthMask(true);
 		RenderSystem.defaultBlendFunc();
 		RenderSystem.enableDepthTest();
 		RenderSystem.disableBlend();
 		RenderSystem.setShaderColor(1, 1, 1, 1);
+	}
+
+	private static void renderEntityInInventoryFollowsAngle(GuiGraphics guiGraphics, int x, int y, int scale, float angleXComponent, float angleYComponent, LivingEntity entity) {
+		Quaternionf pose = new Quaternionf().rotateZ((float) Math.PI);
+		Quaternionf cameraOrientation = new Quaternionf().rotateX(angleYComponent * 20 * ((float) Math.PI / 180F));
+		pose.mul(cameraOrientation);
+		float f2 = entity.yBodyRot;
+		float f3 = entity.getYRot();
+		float f4 = entity.getXRot();
+		float f5 = entity.yHeadRotO;
+		float f6 = entity.yHeadRot;
+		entity.yBodyRot = 180.0F + angleXComponent * 20.0F;
+		entity.setYRot(180.0F + angleXComponent * 40.0F);
+		entity.setXRot(-angleYComponent * 20.0F);
+		entity.yHeadRot = entity.getYRot();
+		entity.yHeadRotO = entity.getYRot();
+		InventoryScreen.renderEntityInInventory(guiGraphics, x, y, scale, new Vector3f(0, 0, 0), pose, cameraOrientation, entity);
+		entity.yBodyRot = f2;
+		entity.setYRot(f3);
+		entity.setXRot(f4);
+		entity.yHeadRotO = f5;
+		entity.yHeadRot = f6;
 	}
 }
