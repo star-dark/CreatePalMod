824d3560e2fe300ba1c8827b2cd0098c52b82f98
ShinSeungHyuck <120080284+ShinSeungHyuck@users.noreply.github.com>
2025-01-29 18:47:34 +0900

    무기만 집중

--- src/main/java/net/mcreator/palworld/init/PalworldModItems.java
+++ src/main/java/net/mcreator/palworld/init/PalworldModItems.java
@@ -6,7 +6,6 @@ package net.mcreator.palworld.init;
 
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.common.DeferredSpawnEggItem;
 
 import net.minecraft.world.item.Item;
 
@@ -49,6 +48,18 @@ import net.mcreator.palworld.item.FarmerSporeItem;
 import net.mcreator.palworld.item.FarmerRootItem;
 import net.mcreator.palworld.item.FarmerDregsItem;
 import net.mcreator.palworld.item.ChunwonItem;
+import net.mcreator.palworld.item.SkyshatterItem;
+import net.mcreator.palworld.item.MjolnirItem;
+import net.mcreator.palworld.item.HeavenItem;
+import net.mcreator.palworld.item.GloryGoldGhostItem;
+import net.mcreator.palworld.item.DainsleifItem;
+import net.mcreator.palworld.item.BurfSwordItem;
+import net.mcreator.palworld.item.BangobuItem;
+import net.mcreator.palworld.item.BalmungItem;
+import net.mcreator.palworld.item.AvalonOfMerlinItem;
+import net.mcreator.palworld.item.AstraBookItem;
+import net.mcreator.palworld.item.AmanomurakumonotsurugiItem;
+import net.mcreator.palworld.item.AegisItem;
 import net.mcreator.palworld.PalworldMod;
 
 public class PalworldModItems {
@@ -98,6 +109,18 @@ public class PalworldModItems {
 	public static final DeferredItem<Item> PIG_SPAWN_EGG = REGISTRY.register("pig_spawn_egg", () -> new DeferredSpawnEggItem(PalworldModEntities.PIG, -1, -1, new Item.Properties()));
 	public static final DeferredItem<Item> QUEEN_SPAWN_EGG = REGISTRY.register("queen_spawn_egg", () -> new DeferredSpawnEggItem(PalworldModEntities.QUEEN, -3407668, -256, new Item.Properties()));
 	public static final DeferredItem<Item> LEVEL_WAND = REGISTRY.register("level_wand", LevelWandItem::new);
+	public static final DeferredItem<Item> BURF_SWORD = REGISTRY.register("burf_sword", BurfSwordItem::new);
+	public static final DeferredItem<Item> SKYSHATTER = REGISTRY.register("skyshatter", SkyshatterItem::new);
+	public static final DeferredItem<Item> MJOLNIR = REGISTRY.register("mjolnir", MjolnirItem::new);
+	public static final DeferredItem<Item> DAINSLEIF = REGISTRY.register("dainsleif", DainsleifItem::new);
+	public static final DeferredItem<Item> BALMUNG = REGISTRY.register("balmung", BalmungItem::new);
+	public static final DeferredItem<Item> BANGOBU = REGISTRY.register("bangobu", BangobuItem::new);
+	public static final DeferredItem<Item> AMANOMURAKUMONOTSURUGI = REGISTRY.register("amanomurakumonotsurugi", AmanomurakumonotsurugiItem::new);
+	public static final DeferredItem<Item> AEGIS = REGISTRY.register("aegis", AegisItem::new);
+	public static final DeferredItem<Item> GLORY_GOLD_GHOST = REGISTRY.register("glory_gold_ghost", GloryGoldGhostItem::new);
+	public static final DeferredItem<Item> ASTRA_BOOK = REGISTRY.register("astra_book", AstraBookItem::new);
+	public static final DeferredItem<Item> AVALON_OF_MERLIN = REGISTRY.register("avalon_of_merlin", AvalonOfMerlinItem::new);
+	public static final DeferredItem<Item> HEAVEN = REGISTRY.register("heaven", HeavenItem::new);
 	// Start of user code block custom items
 	// End of user code block custom items
 }
