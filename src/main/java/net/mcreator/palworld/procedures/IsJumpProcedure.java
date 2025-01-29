3369d6dc5328755dfde238a7b21151c938f34f8e
star-dark <den1116@naver.com>
2025-01-29 15:17:18 +0900

    풀하고 시작

--- src/main/java/net/mcreator/palworld/procedures/IsJumpProcedure.java
+++ src/main/java/net/mcreator/palworld/procedures/IsJumpProcedure.java
@@ -5,6 +5,7 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.bus.api.Event;
 
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.Entity;
 
 import net.mcreator.palworld.network.PalworldModVariables;
@@ -16,21 +17,24 @@ public class IsJumpProcedure {
 	@SubscribeEvent
 	public static void onEntityFall(LivingFallEvent event) {
 		if (event.getEntity() != null) {
-			execute(event, event.getEntity());
+			execute(event, event.getEntity().level(), event.getEntity());
 		}
 	}
 
-	public static void execute(Entity entity) {
-		execute(null, entity);
+	public static double execute(LevelAccessor world, Entity entity) {
+		return execute(null, world, entity);
 	}
 
-	private static void execute(@Nullable Event event, Entity entity) {
+	private static double execute(@Nullable Event event, LevelAccessor world, Entity entity) {
 		if (entity == null)
-			return;
+			return 0;
+		double landMoment = 0;
 		{
 			PalworldModVariables.PlayerVariables _vars = entity.getData(PalworldModVariables.PLAYER_VARIABLES);
 			_vars.JumpVar = true;
 			_vars.syncPlayerVariables(entity);
 		}
+		landMoment = world.dayTime();
+		return landMoment;
 	}
 }
