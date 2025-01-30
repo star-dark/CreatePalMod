988d7caedcef7084a46fb58f8b83c7273cefe3dc
star-dark <den1116@naver.com>
2025-01-30 18:31:14 +0900

    선택과 집중

--- src/main/java/net/mcreator/palworld/item/FarmerRootItem.java
+++ src/main/java/net/mcreator/palworld/item/FarmerRootItem.java
@@ -9,3 +9,14 @@ public class FarmerRootItem extends Item {
 		super(new Item.Properties().stacksTo(32).rarity(Rarity.COMMON));
 	}
 }
+
+package net.mcreator.palworld.item;
+
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+
+public class FarmerRootItem extends Item {
+	public FarmerRootItem() {
+		super(new Item.Properties().stacksTo(32).rarity(Rarity.COMMON));
+	}
+}
--- src/main/java/net/mcreator/palworld/item/FarmerSporeItem.java
+++ src/main/java/net/mcreator/palworld/item/FarmerSporeItem.java
@@ -9,3 +9,14 @@ public class FarmerSporeItem extends Item {
 		super(new Item.Properties().stacksTo(32).rarity(Rarity.COMMON));
 	}
 }
+
+package net.mcreator.palworld.item;
+
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+
+public class FarmerSporeItem extends Item {
+	public FarmerSporeItem() {
+		super(new Item.Properties().stacksTo(32).rarity(Rarity.COMMON));
+	}
+}
