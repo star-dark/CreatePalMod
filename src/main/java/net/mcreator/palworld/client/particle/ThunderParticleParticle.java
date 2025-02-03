b4be97bb169ecbe989e864e772971ed6cd061f5a
ShinSeungHyuck <120080284+ShinSeungHyuck@users.noreply.github.com>
2025-02-03 17:08:49 +0900

    아마노무라쿠모노츠루기 버그 수정

--- src/main/java/net/mcreator/palworld/client/particle/ThunderParticleParticle.java
+++ src/main/java/net/mcreator/palworld/client/particle/ThunderParticleParticle.java
@@ -36,7 +36,8 @@ public class ThunderParticleParticle extends TextureSheetParticle {
 		super(world, x, y, z);
 		this.spriteSet = spriteSet;
 		this.setSize(0.2f, 0.2f);
-		this.lifetime = 7;
+		this.quadSize *= 17.5f;
+		this.lifetime = 25;
 		this.gravity = 0f;
 		this.hasPhysics = true;
 		this.xd = vx * 1;
--- src/main/java/net/mcreator/palworld/procedures/AmanomurakumonotsurugiEffect2Procedure.java
+++ src/main/java/net/mcreator/palworld/procedures/AmanomurakumonotsurugiEffect2Procedure.java
@@ -1,5 +1,10 @@
 package net.mcreator.palworld.procedures;
 
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.Event;
+
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.core.particles.SimpleParticleType;
@@ -7,8 +12,20 @@ import net.minecraft.core.particles.SimpleParticleType;
 import net.mcreator.palworld.network.PalworldModVariables;
 import net.mcreator.palworld.init.PalworldModParticleTypes;
 
+import javax.annotation.Nullable;
+
+@EventBusSubscriber
 public class AmanomurakumonotsurugiEffect2Procedure {
+	@SubscribeEvent
+	public static void onPlayerTick(PlayerTickEvent.Post event) {
+		execute(event, event.getEntity().level(), event.getEntity().getX(), event.getEntity().getY(), event.getEntity().getZ(), event.getEntity());
+	}
+
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		execute(null, world, x, y, z, entity);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
 		if (world.dayTime() <= entity.getData(PalworldModVariables.PLAYER_VARIABLES).Amanomuraku_tick) {
